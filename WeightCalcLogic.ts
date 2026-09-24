8bbe5d91d963fd50947b846fa6ad03a0f55adf1a
Doug Dickens <hellsmaximum@gmail.com>
2026-09-24 14:10:44 -0600

    Created gitignore, and made page for weight calculation

---
+++ WeightCalcLogic.ts
@@ -0,0 +1,18 @@
+const kilogramsToPounds = (kilograms: number): number => kilograms * 2.20462;
+
+const kgInput = document.getElementById("kg-input") as HTMLInputElement;
+const kgButton = document.getElementById("kg-button") as HTMLButtonElement;
+const kgResult = document.getElementById("kg-result") as HTMLParagraphElement;
+
+const handleKgConvert = (): void => {
+  const kilograms: number = Number(kgInput.value);
+  if (kilograms <= 0) {
+    kgResult.textContent = "Please enter a positive number";
+    return;
+  } else{
+    const pounds: number = kilogramsToPounds(kilograms);
+    kgResult.textContent = pounds.toFixed(2);
+  }
+};
+
+kgButton.addEventListener("click", handleKgConvert);
--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
-const kilogramsToPounds = (kilograms: number): number => kilograms * 2.20462;
+// const kilogramsToPounds = (kilograms: number): number => kilograms * 2.20462;
 
-const kgInput = document.getElementById("kg-input") as HTMLInputElement;
-const kgButton = document.getElementById("kg-button") as HTMLButtonElement;
-const kgResult = document.getElementById("kg-result") as HTMLParagraphElement;
+// const kgInput = document.getElementById("kg-input") as HTMLInputElement;
+// const kgButton = document.getElementById("kg-button") as HTMLButtonElement;
+// const kgResult = document.getElementById("kg-result") as HTMLParagraphElement;
 
-const handleKgConvert = (): void => {
-  const kilograms: number = Number(kgInput.value);
-  const pounds: number = kilogramsToPounds(kilograms);
-  kgResult.textContent = pounds.toFixed(2);
-};
-kgButton.addEventListener("click", handleKgConvert);
+// const handleKgConvert = (): void => {
+//   const kilograms: number = Number(kgInput.value);
+//   const pounds: number = kilogramsToPounds(kilograms);
+//   kgResult.textContent = pounds.toFixed(2);
+// };
+// kgButton.addEventListener("click", handleKgConvert);
